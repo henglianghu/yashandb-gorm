a5dd66229c78d9d2dfe4cdf776f64f0d86ce816b
zhusognping <zhusongping@sics.ac.cn>
2023-12-05 10:27:26 +0800

    fix: clause in nil panic&&  gofmt project

--- namer_test.go
+++ namer_test.go
@@ -3,13 +3,13 @@ package yasdb
 import "testing"
 
 func TestTryRemoveQuotes(t *testing.T) {
-    name := []string{`"USER"`, `USER`, `"U"`, `"U`, `U`}
-    wantName := []string{`USER`, `USER`, `U`, `"U`, `U`}
+	name := []string{`"USER"`, `USER`, `"U"`, `"U`, `U`}
+	wantName := []string{`USER`, `USER`, `U`, `"U`, `U`}
 
-    for i, v := range name {
-        getName := TryRemoveQuotes(v)
-        if getName != wantName[i] {
-            t.Fatalf("TryRemoveQuotes function err: name - %s; getName - %s; wantName - %s", v, getName, wantName[i])
-        }
-    }
+	for i, v := range name {
+		getName := TryRemoveQuotes(v)
+		if getName != wantName[i] {
+			t.Fatalf("TryRemoveQuotes function err: name - %s; getName - %s; wantName - %s", v, getName, wantName[i])
+		}
+	}
 }
