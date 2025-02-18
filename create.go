05d9aee88671715a6d864642c718bb7476ce7fed
zhusongping <zhusongping@sics.ac.cn>
2025-02-18 09:48:01 +0800

    feat: update gorm version to 1.25.7

--- create.go
+++ create.go
@@ -85,7 +85,7 @@ func Create(db *gorm.DB) {
 							func(field *gormSchema.Field) {
 								switch insertTo.Kind() {
 								case reflect.Struct:
-									if err = field.Set(insertTo, stmt.Vars[boundVars[field.Name]].(sql.Out).Dest); err != nil {
+									if err = field.Set(stmt.Context, insertTo, stmt.Vars[boundVars[field.Name]].(sql.Out).Dest); err != nil {
 										_ = db.AddError(err)
 									}
 								case reflect.Map:
